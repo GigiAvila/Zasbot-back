bb18dc2a4f4e8df783bc6639d6ea0b7521cd0189
GigiAvila <gigi.s.avila@gmail.com>
2024-06-28 22:32:22 -0300

    change brochure for less heavy one

--- src/controller/brochure.js
+++ src/controller/brochure.js
@@ -1,7 +1,7 @@
 const path = require('path')
 
 const getBrochure = async (req, res, next) => {
-  const brochurePath = path.join(__dirname, '../public/brochure_arg.pdf')
+  const brochurePath = path.join(__dirname, '../public/brochure_tiny.pdf')
   res.sendFile(brochurePath, (err) => {
     if (err) {
       console.error('Error al enviar el archivo PDF:', err)
